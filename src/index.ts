e4ea6762c0f76bc3536f34f49b8a4f8dc4ebf738
taoqf <tao_qiufeng@126.com>
2020-04-03 11:18:49 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
+import global from '@mmstudio/global';
 
-export default function awx20(param1: string, param2: string) {
+interface IFileDoc {
+	id: string;
+	contentType: string;
+	name: string;
+	md5: string;
+}
+
+export default function upload_file(filePath: string, name: string) {
+	return new Promise<IFileDoc>((resolve, reject) => {
+		const host = global('host', 'http://127.0.0.1:8889');
+		const url = `${host}/fsweb/upload`;
+		wx.uploadFile({
+			filePath,
+			name,
+			url,
+			success(res) {
+				const data = res.data;
+				const files = JSON.parse(data) as IFileDoc[];
+				resolve(files[0]);
+			},
+			fail() {
+				reject(false);
+			}
+		});
+	});
 }
